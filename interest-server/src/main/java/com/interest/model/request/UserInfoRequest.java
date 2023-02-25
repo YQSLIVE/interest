7781087130a58b5f0c088dca3d4618ce61496f16
“wangsong” <3088931815@qq.com>
2023-02-25 14:49:55 +0800

    two

--- interest-server/src/main/java/com/interest/model/request/UserInfoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.interest.model.request;
-
-import com.interest.model.response.UserInfoResponse;
-import lombok.Data;
-
-@Data
-public class UserInfoRequest extends UserInfoResponse {
-
-}
--- interest-server/src/main/java/com/interest/service/LoggerService.java
+++
@@ -1,8 +0,0 @@
-package com.interest.service;
-
-import com.interest.annotation.handler.InterestException;
-import com.interest.model.utils.PageResult;
-
-public interface LoggerService {
-    PageResult<InterestException> getLogs(Integer pageIndex, Integer pageSize);
-}
